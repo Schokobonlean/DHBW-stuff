dc005b8d798dbd2892f2a1f8af08970f3598a595
Schokobonlean <95095420+Schokobonlean@users.noreply.github.com>
2022-05-09 15:21:59 +0200

    Add files via upload

---
+++ Semester 1/Methoden und Werkzeuge der SE - Heller/Aufgaben/ugly_project-master/step1/src/main/java/com/dhbw/mdse/ugly_project/App.java
@@ -0,0 +1,17 @@
+package com.dhbw.mdse.ugly_project;
+
+import java.util.Scanner;
+
+public class App {
+    public static  void main(            String[]                     args) {
+        System.out.println("First Number: " + 20);
+        System.out.println("Second Number: " + 5);
+
+
+        int subtraction = 20 - 5;
+        System.out.println("Subtraction: "+subtraction );
+
+        int Mal = 20 * 5;
+        System.out.println("Multiplication: "+                           Mal );
+    }
+}
